4e088d85b3ba33bcae173e1701a3704b62635cdc
Gabriel Kalani <gabrielsilva1956@gmail.com>
2016-07-01 17:18:59 -0300

    Placar funcionando perfeitamente manoooooo

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
 
 import Contador from './Contador';
-import PlacarContainer from './PlacarContainer.js';
+import PlacarContainer from './PlacarContainer';
+
+const dados = {
+	partida: {
+		estadio: "Estádio Maracanã/RJ",
+		data: "01/07/2016",
+		horario: "16h"
+	},
+	casa: {
+		nome: 'Flamengo'
+	},
+	visitante: {
+		nome: 'Corinthians'
+	}
+}
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<PlacarContainer />
-				<Contador />
+				<PlacarContainer partida={dados.partida}
+								 casa={dados.casa}
+								 visitante={dados.visitante} />
 			</div>
 		);
 	}
--- src/components/BotaoGol.js
+++ src/components/BotaoGol.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
 export default class BotaoGol extends Component {
+	handleClick(event) {
+		event.preventDefault();
+		this.props.marcarGol();
+	}
 	render() {
 		return (
-			<button>Foi Gol!</button>
+			<button onClick={this.handleClick.bind(this)}>Gol!</button>
 		);
 	}
 }
