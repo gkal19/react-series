727fc15a3f538f060cbe59e8b62ff0c604a198ca
Gabriel Kalani <gabrielsilva1956@gmail.com>
2016-07-01 16:38:18 -0300

    Placar Estatico

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import Contador from './Contador';
+import PlacarContainer from './PlacarContainer.js';
 
 export default class App extends React.Component {
-    render() {
-      return <Contador />;
-    }
+	render() {
+		return (
+			<div>
+				<PlacarContainer />
+				<Contador />
+			</div>
+		);
+	}
 }
