c3663cbea8e74ce32b1a77dfa2ed6a5c728fd25f
Gabriel Kalani <gabrielsilva1956@gmail.com>
2016-07-01 18:08:29 -0300

    React Router

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route } from 'react-router';
+import { hashHistory } from 'react-router'
 
-import App from './components/App';
+import App from './pages/App';
 
-ReactDOM.render(
-  <App />,
+ReactDOM.render((
+  <Router history={hashHistory} >
+    <Route path="/" component={App} />
+  </Router>
+),
   document.getElementById('app')
 );
